e5ace126cc935770aee815cd9f435e02a1cfdf62
Tushar Pawar <tusharpawar1241@gmail.com>
2026-09-13 22:38:14 +0530

    feat: add QR code generation script and output image

--- server/services/generateQR.py
+++ server/services/generateQR.py
@@ -1,5 +1,5 @@
 import qrcode
-link = "http://localhost:5173/submit"
+link = "https://medikiosk.vercel.app/submit"
 qr = qrcode.make(link)
 qr.save("qr_code.png")
 print("QR Code generated successfully!")
